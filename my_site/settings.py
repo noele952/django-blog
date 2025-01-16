eec3447824d1cbfbc0d3a7a1504ea33341cb39ed
noele952 <noele952@gmail.com>
2025-01-15 16:47:56 -0800

    fix settings.DATABASE

--- my_site/settings.py
+++ my_site/settings.py
@@ -95,8 +95,6 @@ DATABASES = {
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', ''),
     }
-}
-
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
